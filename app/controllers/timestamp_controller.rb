9b39899739ce13314149bee1315ccf45eaaf418b
Tim Millar <heteroskedasticity99@gmail.com>
2016-10-22 11:17:44 +0100

    Changed route to get req with date param

--- app/controllers/timestamp_controller.rb
+++ app/controllers/timestamp_controller.rb
@@ -1,5 +1,9 @@
 class TimestampController < ApplicationController
   def show
-    render json: { foo: "hello world" }
+    render json: {
+             foo: params[:date],
+             unix: Date.today.to_time.to_i,
+             natural: Date.today.strftime("%B, %d, %Y"),
+           }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'timestamp#show'
+
+  get '/:date', to: 'timestamp#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
